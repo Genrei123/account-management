54270387d008c920c11057d3b7d82884c5d2d513
Genrey <109770981+Genrei123@users.noreply.github.com>
2024-08-18 20:54:48 +0800

    Initial commit

---
+++ src/main/java/com/group4/accounting/management/AccountingManagement.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.group4.accounting.management;
+
+/**
+ *
+ * @author User
+ */
+public class AccountingManagement {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
